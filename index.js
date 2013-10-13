6e12c4db1de2eb646e36944e6d5b175f6f5c1858
AWinterman <andywinterman@gmail.com>
2013-10-12 17:27:54 -0700

    Streamified
    Note that this involved some tinkering with attributes marked private. I find
    this unsavory and would love to see an implementation that didn't have this
    wart.

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 module.exports = estate
 
-var EE = require('events').EventEmitter
+var Readable = require('stream').Readable
+  , util = require('util')
+
+util.inherits(Estate, Readable)
 
 function estate() {
   return new Estate()
 }
 
 function Estate() {
-  EE.call(this)
-  this._state = {}
+  var options = {}
+
+  options.objectMode = true
+  options.highWaterMark = 2
+  Readable.call(this, options)
+
+  this.state = {}
+  this.more = true
 }
 
 var cons = Estate
-  , proto = cons.prototype = Object.create(EE.prototype)
+  , proto = cons.prototype
 
 proto.constructor = proto
 
@@ -21,14 +30,29 @@ proto.listen = function(to_ee, on_event, as_attrs) {
 
   to_ee.on(on_event, function() {
     for(var i = 0, len = as_attrs.length; i < len; ++i) {
+
       if(arguments[i] === undefined) {
-        delete self._state[as_attrs[i]]
-      } else {
-        self._state[as_attrs[i]] = arguments[i]
+        delete self.state[as_attrs[i]]
+      }
+
+      if(arguments[i] !== undefined) {
+        self.state[as_attrs[i]] = arguments[i]
       }
     }
-    self.emit('data', self._state)
+
+    self.more = self.unshift(self.state)
+
+    // This feels bad-- the goal is that in the event of back pressure, the
+    // stream still only holds on to the most recent state.
+    if(!self.more) {
+      self._readableState.buffer.pop()
+      self._readableState.length--
+    }
+
   })
 
   return self
+}
+
+proto._read = function() {
 }
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-var estate = require('./index')
-  , EE = require('events').EventEmitter
+var EE = require('events').EventEmitter
+  , estate = require('./index')
   , test = require('tape')
 
 test('works as expected', function(assert) {
@@ -10,34 +10,35 @@ test('works as expected', function(assert) {
   e1.listen(ee1, 'thing', ['one', 'two'])
     .listen(ee2, 'data', ['two', 'three', 'four'])
 
-  e1.once('data', function(state) {
-    assert.deepEqual(state, {
-      one: 1
-    , two: 2
-    })
-  })
 
   ee1.emit('thing', 1, 2)
 
   e1.once('data', function(state) {
     assert.deepEqual(state, {
-      one: 0 
+        one: 1
+      , two: 2
+    })
+  })
+
+
+  e1.once('data', function(state) {
+    assert.deepEqual(state, {
+        one: 0
     })
   })
 
   ee1.emit('thing', 0)
-
 
   // set "one" to 0, "two" to 1
   ee1.emit('thing', 0, 1)
 
   e1.once('data', function(state) {
     assert.deepEqual(state, {
-      one: 0
-    , two: 10
-    , three: 20
+        one: 0
+      , two: 10
+      , three: 20
     })
-  }) 
+  })
 
   ee2.emit('data', 10, 20)
 
@@ -50,3 +51,73 @@ test('works as expected', function(assert) {
 
   assert.end()
 })
+
+
+test('Functions correctly as a pull stream', buffer_doesnt_grow)
+
+function buffer_doesnt_grow(assert) {
+  var state1 = estate()
+    , state2 = estate()
+    , ee = new EE
+    , values = []
+    , i = 0
+
+  values.push('one')
+  values.push('two')
+  values.push('three')
+  values.push('four')
+  values.push('five')
+
+  state1.listen(ee, 'beep', ['beep'])
+  state2.listen(ee, 'beep', ['beep'])
+
+  state1.on('readable', function() {
+    this.read().beep === values[i++]
+  })
+
+  for(var j = 0, len = values.length; j < len; ++j) {
+    ee.emit('beep', values[j])
+    assert.strictEqual(bufferlen(state1), 0)
+    assert.strictEqual(bufferlen(state2), 1)
+  }
+
+  assert.strictEqual(bufferlen(state1), 0)
+  console.log(state2)
+
+  assert.strictEqual(state2.state.beep, 'five')
+  assert.strictEqual(state1.state.beep, 'five')
+  assert.strictEqual(state2.read().beep, 'five')
+
+  assert.end()
+
+  function bufferlen(state) {
+    return state._readableState.buffer.length
+  }
+}
+
+test(
+    'Most recent state is the one that is emitted.'
+  , emits_most_recent
+)
+
+function emits_most_recent(assert) {
+  var obj_state = new estate()
+    , ee = new EE
+
+  obj_state.listen(ee, 'beep', ['beep'])
+
+  ee.emit('beep', 0)
+  ee.emit('beep', 1)
+  ee.emit('beep', 2)
+
+  var second_read
+    , first_read
+
+  // I'm cheating a little, because I know the stream is in a readable state.
+  first_read = obj_state.read()
+  second_read = obj_state.read()
+
+  assert.deepEqual(first_read, {beep: 2})
+  assert.deepEqual(second_read, {beep: 2})
+  assert.end()
+}
